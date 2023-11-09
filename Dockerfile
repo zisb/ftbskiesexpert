83676ec28d6876224c05fd40a53a310ca69a876a
Jason McLeod <jason@mcleods.me>
2023-11-08 20:36:23 -0500

    Update to version 1.3.1

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# syntax=docker/dockerfile:1
+
+FROM openjdk:17.0.2-jdk-buster
+
+LABEL version="1.3.1"
+
+RUN apt-get update && apt-get install -y curl && \
+ adduser --uid 99 --gid 100 --home /data --disabled-password minecraft
+
+COPY launch.sh /launch.sh
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+VOLUME /data
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+ENV MOTD "FTB Skies v1.3.1 Server Powered by Docker"
+ENV LEVEL world
+ENV JVM_OPTS "-Xms4096m -Xmx6144m"
+
+CMD ["/launch.sh"]
---
+++ launch.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -x
+
+if ! [[ -w "/data" ]]; then
+  echo "Directory is not writable, check permissions for /mnt/user/appdata/"
+  exit 66
+fi
+
+ID=103
+VER=6561
+
+cd /data
+
+if ! [[ "$EULA" = "false" ]] || grep -i true eula.txt; then
+	echo "eula=true" > eula.txt
+else
+	echo "You must accept the EULA by in the container settings."
+	exit 9
+fi
+
+if ! [[ -f serverinstall_${ID}_${VER} ]]; then
+  rm -f serverinstall_${ID}* forge-*.jar run.sh start.sh
+  curl -Lo serverinstall_${ID}_${VER} https://api.modpacks.ch/public/modpack/${ID}/${VER}/server/linux
+  chmod +x serverinstall_${ID}_${VER}
+   ./serverinstall_${ID}_${VER} --path /data --nojava
+fi
+
+if [[ -n "$MOTD" ]]; then
+    sed -i "/motd\s*=/ c motd=$MOTD" /data/server.properties
+fi
+if [[ -n "$LEVEL" ]]; then
+    sed -i "/level-name\s*=/ c level-name=$LEVEL" /data/server.properties
+fi
+if [[ -n "$OPS" ]]; then
+    echo $OPS | awk -v RS=, '{print}' > ops.txt
+fi
+if [[ -n "$ALLOWLIST" ]]; then
+    echo $ALLOWLIST | awk -v RS=, '{print}' > white-list.txt
+fi
+
+sed -i 's/server-port.*/server-port=25565/g' server.properties
+
+[[ -f run.sh ]] && chmod 755 run.sh
+[[ -f start.sh ]] && chmod 755 start.sh
+if [[ -f run.sh || -f start.sh ]]; then
+  if [[ -f user_jvm_args.txt ]]; then
+    echo $JVM_OPTS > user_jvm_args.txt
+  fi
+  [[ -f run.sh ]] && ./run.sh || ./start.sh
+else
+  rm -f forge-*-installer.jar
+  FORGE_JAR=$(ls forge-*.jar)
+
+  curl -Lo log4j2_112-116.xml https://launcher.mojang.com/v1/objects/02937d122c86ce73319ef9975b58896fc1b491d1/log4j2_112-116.xml
+  java -server -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -Dfml.queryResult=confirm -Dlog4j.configurationFile=log4j2_112-116.xml $JVM_OPTS -jar $FORGE_JAR nogui
+fi
